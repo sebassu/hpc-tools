53a6daa340a0a1bfeedfc0904500b0d14ce8a2d8
Sebastián Uriarte Güimil <sebastian.uriarteg@gmail.com>
2025-09-26 09:43:13 -0300

    Add provided initial files

---
+++ systems_solver/dgesv.c
@@ -0,0 +1,10 @@
+#include "dgesv.h"
+
+int my_dgesv(int n, int nrhs, double *a, double *b)
+{
+  /* add/change the arguments according to your implementation needs */
+
+  // [...] Write your implementation here
+
+  return 0;
+}
